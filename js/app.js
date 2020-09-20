7f1b9d4f2514485a3e8a9ecd00e2d8edc3a04eb3
Diego09 <contac@diego09.com>
2020-09-19 23:04:36 -0500

    Diseño Reponsive

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const checkbox = document.querySelector("#checkbox")
+if (window.matchMedia("(prefers-color-scheme: dark)").metches){
+    checkbox.setAttribute("checked",true)
+}
+
+document.body.style.setProperty("--cardGray", "")
+checkbox.addEventListener("change", function (event) {
+    document.body.classList.add("is-dark-mode")
+    if (this.checked) {
+        document.body.classList.remove("is-light-mode")
+        document.body.classList.add("is-dark-mode")
+    } else {
+        document.body.classList.remove("is-dark-mode")
+        document.body.classList.add("is-light-mode")
+    }
+})
